f30edaa7953865b97b4f06b7ac4e80b8c57ee8dd
AgustinVimer <avimercatticab@gmail.com>
2022-09-18 19:00:43 -0300

    entrega 3 final
    todo lo restante en entrega 3, deje pila de cosas comentadas para lo de mandar un mensaje y que se muestre

--- js/product-info.js
+++ js/product-info.js
@@ -38,7 +38,7 @@ function mostrarInfoProducto() {
                 
              
                 <div class="col">
-                    <img src="${imagen}" class="bd-placeholder-img card-img-top> 
+                    <img src="${imagen}" class="bd-placeholder-img card-img-top"> 
                 </div>`
 
     }
@@ -65,28 +65,103 @@ function mostrarListaComentarios(){
         let comentario = listaProductInfoComentarios[i];
         
         
-        comentariosHtml += `<p><b> ${comentario.user} </b>${comentario.dateTime} ${comentario.score}</p>
+        comentariosHtml += `<p><b> ${comentario.user} </b>${comentario.dateTime} ${cantEstrellas(comentario)}</p>
         <p> ${comentario.description}</p>`
      
      
       
     }
 
-       comentariosHtml += `<br>
+   /*  comentariosHtml+= `<p><b> ${localStorage.getItem("loginUsuario")} </b>${localStorage.getItem("fecha")} ${cantEstrellas(localStorage.getItem("stars"))}</p>
+            <p> ${localStorage.getItem("coment")}</p>` */
+
+    /*    comentariosHtml += `<br>
         <div class="mb-3">
         <label for="exampleFo>rmControlTextarea1" class="form-label">Añade un comentario:</label>
         <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
-      <button type="button" class="btn btn-primary">Enviar comentario</button>
+        </div>
+        <div class="input-group mb-3">
+         <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Puntaje</label>
+        </div>
+  <select class="custom-select  required" id="inputGroupSelect01">
+    <option value= "0" selected disabled="disabled">Puntúa al producto</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+  </select>
+</div>
+      <button onclick="enviarComentario()" type="button" class="btn btn-primary">Enviar comentario</button>
         `
+ */
+        
          
 
     document.getElementById("comentarios").innerHTML = comentariosHtml; 
+    /* localStorage.setItem("comentariosHtml", comentariosHtml) */
 }
     
+function cantEstrellas(comentario){
+    let puntos = comentario.score;
+    let html = "";
 
+    if (puntos == 1) {
+    html +=`<span class="fa fa-star checked"></span>
+    <span class="fa fa-star"></span>
+    <span class="fa fa-star"></span>
+    <span class="fa fa-star"></span>
+    <span class="fa fa-star"></span>`
+    }
+    else if (puntos == 2){
+        html +=   `<span class="fa fa-star checked"></span>
+     <span class="fa fa-star checked"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>
+     <span class="fa fa-star"></span>`
+    }
+    else if (puntos == 3)
+    { html +=   `<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>`
+    }
+    else if (puntos == 4)
+    { html +=  `<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>`
+    }
+    else 
+    { html +=  `<span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>`
+    }
 
+    return html;
 
+    }
+
+   /* function enviarComentario(){
+
+        let comen = document.getElementById("exampleFormControlTextarea1").value;
+        let stars = document.getElementById("inputGroupSelect01").value;
+        let fecha = new Date(); 
+        let fechaHoy = fecha.getFullYear() + "-" + fecha.getMonth() + "-" + fecha.getDate() + " "+ fecha.getHours() + ":" + fecha.getMinutes()+ ":" + fecha.getSeconds(); 
+        localStorage.setItem("coment", comen);
+        localStorage.setItem("stars", stars);
+        localStorage.setItem("fecha",fechaHoy );
+        comentariosHtml = localStorage.getItem("comentariosHtml") */
+            
+        
+
+          
+        
 
 document.addEventListener("DOMContentLoaded", function () {
     prodID = localStorage.getItem("prodID")
@@ -104,57 +179,31 @@ document.addEventListener("DOMContentLoaded", function () {
             mostrarListaComentarios();
         }
     });
-});
 
-
-/*
-function cantEstrellas(puntos){
-    let puntos = comentario.score
-
-    if (puntos == 1) {
-    `<span class="fa fa-star checked"></span>
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>
-    <span class="fa fa-star"></span>`
-    }
-    else if (puntos == 2){
-        `<span class="fa fa-star checked"></span>
-     <span class="fa fa-star checked"></span>
-     <span class="fa fa-star"></span>
-     <span class="fa fa-star"></span>
-     <span class="fa fa-star"></span>`
-    }
-    else if (puntos == 3)
-    {   `<span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star"></span>
-        <span class="fa fa-star"></span>`
-    }
-    else if (puntos == 4)
-    {   `<span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star"></span>`
-    }
-    else 
-    {   `<span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>
-        <span class="fa fa-star checked"></span>`
-    }
-
-    }
-    */
-
-
-
-/* getJSONData(PRODUCT_INFO_COMMENTS_URL+ prodID + ".json").then(function(resultObj){
-        if (resultObj.status === "ok"){
-            listaProductInfoComentarios = resultObj.data;
-        }
+    document.getElementById("enviarComentario").addEventListener("click", function(){
+        if (parseInt(document.getElementById("inputGroupSelect01").value) == 0 
+        || document.getElementById("anadeComentario").value === "" ){         
+            alert("Debes seleccionar un puntaje y escribir un comentario")
+        } 
+        else{
+            window.location.href = "product-info.html";
+            
+            }
+           
     });
-    */
+
+    
+    
+    
+    });
+
+
+
+
+
+
+
+    
+
+
+
