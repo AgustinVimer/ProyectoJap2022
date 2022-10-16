8372a6c61d902f0eb1f9750a0674b68981c6132b
AgustinVimer <avimercatticab@gmail.com>
2022-10-16 19:39:16 -0300

    Entrega 5
    Todos los cambios para la entrega 5

---
+++ js/cart.js
@@ -0,0 +1,65 @@
+const userID = "25801"
+let articulosArray = []
+
+
+function cambiarInput(idArticulo, costo, nuevaCantidad){
+let costoTotalArticulo = document.getElementById("id"+ idArticulo);
+let nuevoCostoTotal = totalCost (costo, nuevaCantidad);
+costoTotalArticulo.innerHTML = nuevoCostoTotal;
+  
+   } 
+
+function totalCost(costo, cantidad){
+    let totalCost = costo * cantidad;
+    return totalCost;
+}
+
+function mostrarCarrito(){
+
+    let htmlCarrito = "";
+    for(let i = 0; i < articulosArray.length; i++){
+        let articles = articulosArray[i];
+
+        
+    
+
+            htmlCarrito += `
+            <th><img class="imgCarrito" src="${articles.image}"></th>
+            <td>${articles.name}</td>
+            <td id="unitCost"><b>${articles.currency}</b> ${articles.unitCost}</td>
+            <td id="cantCarrito"><input type="number" min="0" id="${articles.id}" class="cant" placeholder="1" value="${articles.count}
+                                  "onchange="cambiarInput(${i},${articles.unitCost}, this.value)"></input></td>
+            <td id="currency"><b>${articles.currency}</b> </td>
+            <td id="id${i}"> ${articles.unitCost}</td>
+            
+            `
+
+
+            
+        }
+
+        document.getElementById("bodyTabla").innerHTML += htmlCarrito;
+    }
+
+ 
+
+
+  
+
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(CART_INFO_URL+ userID + ".json").then(function(resultObj){
+        if (resultObj.status === "ok"){
+            articulosArray = resultObj.data.articles             
+         localStorage.setItem("listaCarrito",JSON.stringify(articulosArray));
+            mostrarCarrito()
+           
+        }
+
+    
+        
+    });
+
+});
+
+
+
